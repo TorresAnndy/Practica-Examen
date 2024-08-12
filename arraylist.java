e2e9c04e8a0d1efd52bded8d62db20c5af2d443a
TorresAnndy <andygonzales2003@gmail.com>
2024-08-11 23:05:36 -0500

    arraylist

---
+++ arraylist.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class arraylist {
+  public static void main(String[] args) {
+    List<Persona> lista = new ArrayList<Persona>();
+    lista.add(new Persona(1, "Andy", 21));
+    lista.add(new Persona(2, "Aylin", 19));
+    lista.add(new Persona(3, "Andres", 18));
+    lista.add(new Persona(4, "Angel", 21));
+
+    System.out.println("-----for-----");
+    //recorrer por indice
+    for(int i = 0; i < lista.size(); i++){
+      System.out.println(lista.get(i).getNombre());
+    }
+
+    System.out.println("-----foreach-----");
+
+    //recorrido foreach
+    for (Persona persona : lista) {
+      System.out.println("Num. "+ persona.getNum());
+      System.out.println("Nombes. "+ persona.getNombre());
+      System.out.println("Edades. "+ persona.getEdad());
+      System.out.println("\n");
+      
+    }
+  }
+}
