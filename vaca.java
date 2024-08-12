4cb8af7414801b928a42ff08b53b1874ba82729e
TorresAnndy <andygonzales2003@gmail.com>
2024-08-11 22:44:22 -0500

    new file:   vaca.java 	new file:   x.java

---
+++ vaca.java
@@ -0,0 +1,31 @@
+public class vaca {
+  public static void impresionvaca()
+  {
+    System.out.println("          (__)");
+    System.out.println("          (uu)");
+    System.out.println("  /------- \\/");
+    System.out.println(" / |     ||");
+    System.out.println("*  ||----||");
+    System.out.println("   ~~     ~~");
+  }
+
+  public static void vacafor(){
+    String[] vaca = {"          (__)",
+    "          (uu)",
+    "  /------- \\/",
+    " / |     ||",
+    "*  ||----||",
+    "   ~~     ~~",
+    };
+
+    for (int i = 0; i<vaca.length; i++){
+      System.out.println(vaca[i]);
+    }
+
+  }
+
+  public static void main(String[] args) {
+    vacafor();
+  }
+  
+}
---
+++ x.java
@@ -0,0 +1,21 @@
+public class x {
+
+  public static void x(){
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        if (j == i || j == (9 - 1 - i)) {
+          System.out.print("*");
+        }
+        else{
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    x();
+  }
+  
+}
